b1d5767613dc087acb85658a1a9e414caec52b39
moisesbaldocchi <128741613+moisesbaldocchi@users.noreply.github.com>
2023-03-30 20:29:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let interruptor = document.getElementById('interruptor')
+let root = document.documentElement
+
+interruptor.addEventListener('click', () => {
+root.classList.toggle('tema-claro')
+})
+
